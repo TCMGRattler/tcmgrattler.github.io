b8f2535cbc61ebebc454e2e8841db0d98a56c18b
TCMGRattler <alextionloc@gmail.com>
2026-09-09 19:44:39 -0500

    data.js

--- data.js
+++ data.js
@@ -13,7 +13,7 @@ const DATA =
     "secondPlace": 730,
     "thirdPlace": 485
   },
-  "lastUpdated": "2026-09-09T19:30:08.000Z",
+  "lastUpdated": "2026-09-09T19:44:37.000Z",
   "flights": [
     {
       "flight": 1,
@@ -1878,7 +1878,7 @@ const DATA =
   ],
   "_source": {
     "file": "2026 MG Master Spreadsheet v1.xlsx",
-    "generated": "2026-09-09T19:30:08.000Z"
+    "generated": "2026-09-09T19:44:37.000Z"
   },
   "_dataCheck": {
     "flightCount": 10,
